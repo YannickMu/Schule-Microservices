5d0194c0695430ac73e16592ec6b2019201ae5c7
Yannick Müller <info@yannickmueller.dev>
2025-05-14 14:19:54 +0200

    added healthcheck for mongodb, depends_on and envvar for connectionString for webapi
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,16 +1,18 @@
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!\r\n");
 
 
-var mongoClient = new MongoClient("mongodb://gbs:geheim@mongodb:27017");
-app.MapGet("/check", async () =>
+app.MapGet("/check", async (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) =>
 {
     try
     {
+        var mongoClient = new MongoClient(options.Value.ConnectionString);
         var databases = await mongoClient.ListDatabaseNamesAsync();
         var databaseNames = await databases.ToListAsync();
         string responseString = "MongoDB connection ok: ";
