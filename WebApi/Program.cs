e5f72f74bfa926043e1af6e1d0d0d9a21cea6544
Yannick Müller <info@yannickmueller.dev>
2025-05-14 13:51:17 +0200

    added mongodb to docker-compose file and /check route
    Signed-off-by: Yannick Müller <info@yannickmueller.dev>

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!\r\n");
+
+
+var mongoClient = new MongoClient("mongodb://gbs:geheim@mongodb:27017");
+app.MapGet("/check", async () =>
+{
+    try
+    {
+        var databases = await mongoClient.ListDatabaseNamesAsync();
+        var databaseNames = await databases.ToListAsync();
+        string responseString = "MongoDB connection ok: ";
+        foreach (var db in databaseNames)
+        {
+            responseString += db + ", ";
+        }
+        return responseString.Remove(responseString.Length - 2);
+    }
+    catch (Exception ex)
+    {
+        return $"Error fetching databases: {ex.Message}";
+    }
+});
 
 app.Run();
